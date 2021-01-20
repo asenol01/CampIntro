e04779a7eb6b2edcfbad5eabf8fc55401b489324
abdis <abdisenol@gmail.com>
2021-01-20 23:50:55 +0300

    2001

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "ali","murta", "kerem", "halil"};
+            //Console.WriteLine(isimler[0]); // Bu yöntemde araya ekleme yapma zor ve sona da. Bunun yerine List kullanılacak
+           
+            // ctrl+k ctrl+c    ya da ctrl+k ctrl+u
+            //List bir sınıftır. İlk yazınca solda çıkan ampule tıklanır ve System.Collections.Generic otomatik eklenir.
+
+            List<string> isimler2 = new List<string> { "ali", "murta", "kerem", "halil" };
+            Console.WriteLine(isimler2[0]);
+            isimler2.Add("veli");
+
+            Console.WriteLine(isimler2[4]);
+        }
+    }
+}
