cc3ab2a4667af1e99ae616cf0065f37a1c2b5c98
abdis <abdisenol@gmail.com>
2021-01-20 22:01:54 +0300

    2001

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //aynı (string ad) gibi
+        public void Add(Product product)
+        {
+            Console.WriteLine(product);
+
+        }
+    }
+}
