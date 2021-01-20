0ec94091254119422f74c40be00ace104f9b0851
abdis <abdisenol@gmail.com>
2021-01-20 16:59:13 +0300

    20/01

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void Ekle(Musteri musteri)
+        {
+        }
+        public void Sil(Musteri musteri)
+        {
+        }
+        public void Listele(Musteri musteri)
+        {
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Ali";
+            musteri1.Soyadi = "GEL";
+            musteri1.Yasi = 40;
+
+            Musteri musteri2 = new Musteri();
+            musteri1.Id = 2;
+            musteri2.Adi = "Veli";
+            musteri2.Soyadi = "GIT";
+            musteri2.Yasi = 21;
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.Yasi);
+                musteriManager.Listele(musteri);
+                Console.WriteLine("---------------------------");
+
+            }
+            
+            //musteriManager.Ekle(musteri1);
+            //musteriManager.Sil(musteri2);
+        }
+    }
+}
