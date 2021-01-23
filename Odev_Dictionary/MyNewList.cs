e8a7dc24d61e8e5bc5d4191093f40fc4a41241cb
abdis <abdisenol@gmail.com>
2021-01-23 20:20:44 +0300

    2301

--- Odev_Dictionary/MyNewList.cs
+++ Odev_Dictionary/MyNewList.cs
@@ -2,28 +2,35 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace GenericsIntro
+namespace Odev_Dictionary
 {
-    //T ifadesi type dan geliyor
-    class MyList<T>
-    {    
+    class MyNewList<T>
+    {
         T[] items;
-        //ctor --- tab tab
-        public MyList()
+        public MyNewList()
         {
             items = new T[0];
         }
-    
-        public void Add (T item)
+
+        public void Add(T item)
         {
             T[] tempArray = items;
             items = new T[items.Length + 1];
-
             for (int i = 0; i < tempArray.Length; i++)
             {
                 items[i] = tempArray[i];
             }
+
             items[items.Length - 1] = item;
+
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine(items[i] + " ");
+            }
         }
     }
 }
---
+++ Odev_Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Odev_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyNewList<string> myNewList = new MyNewList<string>();
+            myNewList.Add("A");
+            myNewList.Add("B");
+            myNewList.Add("C");
+            myNewList.Show();
+            Console.ReadLine();
+        }
+    }
+}
